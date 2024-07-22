d9958747a60bc1e9de4ee1f871ac019e01f5954c
chiton112 <lethanhdat032755@gmail.com>
2024-07-23 00:28:37 +0700

    Initial commit

---
+++ src/main/java/com/iuh/webthietbiamthanh/WebThietBiAmThanhApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.webthietbiamthanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThietBiAmThanhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebThietBiAmThanhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iuh/webthietbiamthanh/WebThietBiAmThanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.webthietbiamthanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThietBiAmThanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
