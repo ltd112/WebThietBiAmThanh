07396185e82f54c597279ed94845a6c599a83699
chiton112 <lethanhdat032755@gmail.com>
2024-07-23 14:36:10 +0700

    cap nhat mot so thu

--- src/main/java/com/iuh/webthietbiamthanh/models/Product.java
+++ src/main/java/com/iuh/webthietbiamthanh/models/Product.java
@@ -1,4 +1,18 @@
 package com.iuh.webthietbiamthanh.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 }
--- src/main/java/com/iuh/webthietbiamthanh/repository/OrderRepository.java
+++ src/main/java/com/iuh/webthietbiamthanh/repository/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.iuh.webthietbiamthanh.repository;
 
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface OrderRepository {
 }
