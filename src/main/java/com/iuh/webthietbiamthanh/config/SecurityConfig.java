17b85756f0a9b126289eb0b5ccf41adc48212429
dinhlong28 <20122841.long@student.iuh.edu.vn>
2024-07-26 23:29:50 +0700

    update handler khi đăng nhập tự động điều hướng tới user hoặc admin

--- src/main/java/com/iuh/webthietbiamthanh/config/SecurityConfig.java
+++ src/main/java/com/iuh/webthietbiamthanh/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.iuh.webthietbiamthanh.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -8,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -33,11 +37,12 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable()).cors(cors->cors.disable())
                 .authorizeHttpRequests(req->req.requestMatchers("/user/**").hasRole("USER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().permitAll())
+                        .requestMatchers("/").permitAll())
                 .formLogin(form->form.loginPage("/signin")
                         .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/")).logout(logout->logout.permitAll());
+//                        .defaultSuccessUrl("/"))
+                        .successHandler(authenticationSuccessHandler))
+                .logout(logout->logout.permitAll());
         return http.build();
     }
 }
